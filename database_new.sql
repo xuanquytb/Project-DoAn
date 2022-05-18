540f48bdbe0a64b7fe2631725a453ccc7e9fd912
xuanquytb <vuxuanquynnn@gmail.com>
2022-05-18 10:00:04 +0700

    add database function and view product

--- database_new.sql
+++ database_new.sql
@@ -66,7 +66,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (2,'Nhà cửa đời sống','default.png','Bao gồm các sản phẩm xung quanh đời sống'),(3,'Thiết bị gia dụng','default.png','default.jpg'),(4,'Thiết bị nhà bếp','default.png','default.jpg'),(5,'Balo & Túi ví','image-1652503330139.jpeg','default.jpg'),(13,'Test ngành hàng','image-1652522272332.png','456464');
+INSERT INTO `category` VALUES (1,'Nhà cửa đời sống','default.png','Bao gồm các sản phẩm xung quanh đời sống'),(2,'Thiết bị gia dụng','default.png','default.jpg'),(3,'Thiết bị nhà bếp','default.png','default.jpg'),(4,'Balo & Túi ví','image-1652503330139.jpeg','default.jpg'),(5,'Test ngành hàng','image-1652522272332.png','456464');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -183,7 +183,7 @@ CREATE TABLE `manufacturer` (
 
 LOCK TABLES `manufacturer` WRITE;
 /*!40000 ALTER TABLE `manufacturer` DISABLE KEYS */;
-INSERT INTO `manufacturer` VALUES (1,'Sony','7453743754','768676786','76786786','image-1652520600435.jpeg'),(7,'Xiaomi','0984646516','trung quốc','xiaomi@gmail.com','image-1652522550611.jpeg');
+INSERT INTO `manufacturer` VALUES (1,'Sony','7453743754','768676786','76786786','image-1652520600435.jpeg'),(2,'Xiaomi','0984646516','trung quốc','xiaomi@gmail.com','image-1652522550611.jpeg');
 /*!40000 ALTER TABLE `manufacturer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -230,7 +230,7 @@ CREATE TABLE `origin` (
   `description` text NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nameOrigin` (`nameOrigin`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -239,7 +239,7 @@ CREATE TABLE `origin` (
 
 LOCK TABLES `origin` WRITE;
 /*!40000 ALTER TABLE `origin` DISABLE KEYS */;
-INSERT INTO `origin` VALUES (1,'Việt Nam','Việt nam');
+INSERT INTO `origin` VALUES (1,'Việt Nam','Việt nam'),(2,'Trung Quốc','Trung Quốc');
 /*!40000 ALTER TABLE `origin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -258,7 +258,7 @@ CREATE TABLE `product` (
   `quantity` int NOT NULL,
   `price` double NOT NULL,
   `promotional` double NOT NULL,
-  `status` int NOT NULL,
+  `status` text NOT NULL,
   `image` varchar(150) NOT NULL,
   `idCategory` int NOT NULL,
   `idUnit` int NOT NULL,
@@ -274,7 +274,7 @@ CREATE TABLE `product` (
   CONSTRAINT `product_ibfk_2` FOREIGN KEY (`idUnit`) REFERENCES `unit` (`id`),
   CONSTRAINT `product_ibfk_3` FOREIGN KEY (`idManufacturer`) REFERENCES `manufacturer` (`id`),
   CONSTRAINT `product_ibfk_4` FOREIGN KEY (`idOrigin`) REFERENCES `origin` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -283,8 +283,33 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (7,'Máy sấy tóc','<p><span style=\"color: rgba(0, 0, 0, 0.8);\">V❤️chất liệu sản phẩm Tici</span></p><p><span style=\"color: rgba(0, 0, 0, 0.8);\">#aonuthoitrang ? Hàng áo nữ của shop là hàng freesize</span></p><p><span style=\"color: rgba(0, 0, 0, 0.8);\">?Chất co giãn thoải mái, đậm nữ tính</span></p><p><span style=\"color: rgba(0, 0, 0, 0.8);\">From nữ ?????</span></p><p><span style=\"color: rgba(0, 0, 0, 0.8);\">- áo : cao m62 nặng 55kg đổ lại là đẹp nhé?                          </span></p><p><span style=\"color: rgba(0, 0, 0, 0.8);\">?Thiết kế với màu như ảnh: </span></p><p><span style=\"color: rgba(0, 0, 0, 0.8);\">?Các sp của shop đa số đều có đăng kèm ảnh thật sp</span></p><p><span style=\"color: rgba(0, 0, 0, 0.8);\">?Khi đặt hàng các bạn đọc kỹ thông tin sp và đặt đơn với đúng màu và số lượng để tránh gửi hàng nhầm lẫn ạ.?</span></p><p><span style=\"color: rgba(0, 0, 0, 0.8);\">❗️shop không nhận đặt màu qua lưu ý và ib mong các bạn thông cảm.</span></p><p><span style=\"color: rgba(0, 0, 0, 0.8);\">??????????????????</span></p>',12,300,20000000,10,'Hết hàng','hinh-anh-avatar-nam-1-600x600.jpg',1,2,1,2);
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `productview`
+--
+
+DROP TABLE IF EXISTS `productview`;
+/*!50001 DROP VIEW IF EXISTS `productview`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `productview` AS SELECT 
+ 1 AS `id`,
+ 1 AS `nameProduct`,
+ 1 AS `description`,
+ 1 AS `warranty`,
+ 1 AS `quantity`,
+ 1 AS `price`,
+ 1 AS `promotional`,
+ 1 AS `status`,
+ 1 AS `image`,
+ 1 AS `nameCategory`,
+ 1 AS `nameUnit`,
+ 1 AS `nameManufacturer`,
+ 1 AS `nameOrigin`*/;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `role`
@@ -325,7 +350,7 @@ CREATE TABLE `unit` (
   `description` text NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nameUnit` (`nameUnit`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -334,7 +359,7 @@ CREATE TABLE `unit` (
 
 LOCK TABLES `unit` WRITE;
 /*!40000 ALTER TABLE `unit` DISABLE KEYS */;
-INSERT INTO `unit` VALUES (1,'Chiếc','1');
+INSERT INTO `unit` VALUES (1,'Chiếc','1'),(2,'Thùng','2');
 /*!40000 ALTER TABLE `unit` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -387,6 +412,73 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'webthaotran'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `updateProduct` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updateProduct`(
+IN fid INT,
+IN fnameProduct VARCHAR(200),
+IN fdescription TEXT,
+IN fwarranty INT,
+IN fquantity INT, 
+IN fpromotional DOUBLE, 
+IN fprice DOUBLE,
+IN fstatus TEXT,
+IN fidCategory nvarchar(200),
+IN fnameManufacturer nvarchar(200),
+IN forigin nvarchar(200)
+)
+BEGIN
+	declare idManu int;
+    declare idOri int;
+    declare idCategory int;
+    declare idUnit int;
+	select id INTO idManu from manufacturer where nameManufacturer like fnameManufacturer;
+    select id INTO idOri from origin where nameOrigin like forigin;
+    select id INTO idCategory from category where nameCategory like fidCategory;
+
+    Update product SET 
+    nameProduct = fnameProduct, 
+    description = fdescription, 
+    warranty = fwarranty , 
+    quantity = fquantity,
+    price = fprice,
+    promotional = fpromotional, 
+    status = fstatus , 
+    idCategory = idCategory, 
+    idManufacturer = idManu , 
+    idOrigin = idOri WHERE (id = fid);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Final view structure for view `productview`
+--
+
+/*!50001 DROP VIEW IF EXISTS `productview`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `productview` AS select `product`.`id` AS `id`,`product`.`nameProduct` AS `nameProduct`,`product`.`description` AS `description`,`product`.`warranty` AS `warranty`,`product`.`quantity` AS `quantity`,`product`.`price` AS `price`,`product`.`promotional` AS `promotional`,`product`.`status` AS `status`,`product`.`image` AS `image`,`category`.`nameCategory` AS `nameCategory`,`unit`.`nameUnit` AS `nameUnit`,`manufacturer`.`nameManufacturer` AS `nameManufacturer`,`origin`.`nameOrigin` AS `nameOrigin` from ((((`product` join `manufacturer` on((`product`.`idManufacturer` = `manufacturer`.`id`))) join `origin` on((`product`.`idOrigin` = `origin`.`id`))) join `category` on((`product`.`idCategory` = `category`.`id`))) join `unit` on((`product`.`idUnit` = `unit`.`id`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -397,4 +489,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-15 17:26:42
+-- Dump completed on 2022-05-18  9:57:37
