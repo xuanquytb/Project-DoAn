bed0a62c0ed27001a2ed84ac3d4e1bcab3c4dabf
xuanquytb <vuxuanquynnn@gmail.com>
2022-05-19 16:28:15 +0700

    fix bug style of category-user --- HUY

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -11,113 +11,72 @@ import ProductContextProvider from "./Store/Context/ProductContext";
 import CategoryContextProvider from "./Store/Context/CategoryContext";
 import BrandContextProvider from "./Store/Context/BrandContext";
 import ProtectedRoute from "./Store/Routing/ProtectedRoute";
-import Category from "./Components/layout/Page/Category";
+import Category_User from "./Components/layout/Page/Category_User";
 
 function App() {
-    return (
-        <AuthContextProvider>
-            <UserContextProvider>
-                <CategoryContextProvider>
-                    <BrandContextProvider>
-                        <ProductContextProvider>
-                            <Router>
-                                <Switch>
-                                    <Route exact path='/' component={Landing} />
-                                    <Route
-                                        exact
-                                        path='/login'
-                                        render={(props) => (
-                                            <Auth
-                                                {...props}
-                                                authRoute='login'
-                                            />
-                                        )}
-                                    />
-                                    <Route
-                                        exact
-                                        path='/register'
-                                        render={(props) => (
-                                            <Auth
-                                                {...props}
-                                                authRoute='register'
-                                            />
-                                        )}
-                                    />
-                                    <Route
-                                        exact
-                                        path='/admin/login'
-                                        render={(props) => (
-                                            <Auth
-                                                {...props}
-                                                authRoute='loginAdmin'
-                                            />
-                                        )}
-                                    />
+  return (
+    <AuthContextProvider>
+      <UserContextProvider>
+        <CategoryContextProvider>
+          <BrandContextProvider>
+            <ProductContextProvider>
+              <Router>
+                <Switch>
+                  <Route exact path="/" component={Landing} />
+                  <Route
+                    exact
+                    path="/login"
+                    render={(props) => <Auth {...props} authRoute="login" />}
+                  />
+                  <Route
+                    exact
+                    path="/register"
+                    render={(props) => <Auth {...props} authRoute="register" />}
+                  />
+                  <Route
+                    exact
+                    path="/admin/login"
+                    render={(props) => (
+                      <Auth {...props} authRoute="loginAdmin" />
+                    )}
+                  />
 
-                                    <Route
-                                        exact
-                                        path='/checkout'
-                                        component={Checkout}
-                                    />
-                                    <ProtectedRoute
-                                        exact
-                                        path='/dashboard'
-                                        component={Dashboard}
-                                    />
-                                    <ProtectedRoute
-                                        exact
-                                        path='/user'
-                                        component={Dashboard}
-                                    />
-                                    <ProtectedRoute
-                                        exact
-                                        path='/admin'
-                                        component={Dashboard}
-                                    />
-                                    <ProtectedRoute
-                                        exact
-                                        path='/employee'
-                                        component={Dashboard}
-                                    />
-                                    <ProtectedRoute
-                                        exact
-                                        path='/category'
-                                        component={Dashboard}
-                                    />
-                                    <ProtectedRoute
-                                        exact
-                                        path='/product'
-                                        component={Dashboard}
-                                    />
-                                    <ProtectedRoute
-                                        exact
-                                        path='/brand'
-                                        component={Dashboard}
-                                    />
-                                    <ProtectedRoute
-                                        exact
-                                        path='/new'
-                                        component={Dashboard}
-                                    />
-                                    <ProtectedRoute
-                                        exact
-                                        path='/comment'
-                                        component={Dashboard}
-                                    />
-                                    <ProtectedRoute
-                                        exact
-                                        path='/category-user'
-                                        component={Category}
-                                    />
-                                    <Route path='/notfound' />
-                                </Switch>
-                            </Router>
-                        </ProductContextProvider>
-                    </BrandContextProvider>
-                </CategoryContextProvider>
-            </UserContextProvider>
-        </AuthContextProvider>
-    );
+                  <Route exact path="/checkout" component={Checkout} />
+                  <ProtectedRoute
+                    exact
+                    path="/dashboard"
+                    component={Dashboard}
+                  />
+                  <ProtectedRoute exact path="/user" component={Dashboard} />
+                  <ProtectedRoute exact path="/admin" component={Dashboard} />
+                  <ProtectedRoute
+                    exact
+                    path="/employee"
+                    component={Dashboard}
+                  />
+                  <ProtectedRoute
+                    exact
+                    path="/category"
+                    component={Dashboard}
+                  />
+                  <ProtectedRoute exact path="/product" component={Dashboard} />
+                  <ProtectedRoute exact path="/brand" component={Dashboard} />
+                  <ProtectedRoute exact path="/new" component={Dashboard} />
+                  <ProtectedRoute exact path="/comment" component={Dashboard} />
+                  <Route
+                    exact
+                    path="/category-user"
+                    component={Category_User}
+                  />
+                  <Route path="/notfound" />
+                </Switch>
+              </Router>
+            </ProductContextProvider>
+          </BrandContextProvider>
+        </CategoryContextProvider>
+      </UserContextProvider>
+    </AuthContextProvider>
+  );
 }
 
 export default App;
